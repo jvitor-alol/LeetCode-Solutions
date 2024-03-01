8d368ec372b93117a303041447e3ef8e7a837543
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-03-01 15:35:38 -0300

    2877

---
+++ 2877. Create a DataFrame from List/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from typing import List
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    df = pd.DataFrame(student_data, columns=['student_id', 'age'])
+    print(df)
+    return df
+    
+def main():
+    createDataframe([[1,15],[2,11],[3,11],[4,20]])
+    
+if __name__=="__main__":
+    main()
