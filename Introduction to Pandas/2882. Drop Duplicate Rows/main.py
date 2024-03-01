3350bd82f0ade9d468268b5ac66a9ad2fda2876e
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-03-01 16:25:29 -0300

    2882

---
+++ Introduction to Pandas/2882. Drop Duplicate Rows/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from typing import List
+    
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    customers = customers.drop_duplicates(subset=['email'])
+    return customers
+
+def main():
+    data = {
+        'customer_id': [1, 2, 3, 4, 5, 6],
+        'name': ['Ella', 'David', 'Zachary', 'Alice', 'Finn', 'Violet'],
+        'email': ['emily@example.com', 'michael@example.com', 'sarah@example.com', 'john@example.com', 'john@example.com', 'alice@example.com']
+    }
+    df = pd.DataFrame(data)
+    print(dropDuplicateEmails(df))
+    
+if __name__=="__main__":
+    main()
